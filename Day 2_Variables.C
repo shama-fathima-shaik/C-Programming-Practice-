dfca9059f8198cc363d7791995ecc6a5d77597d0
SHAMA FATHIMA SHAIK <shaikshamafathima@gmail.com>
2026-06-12 22:52:27 +0530

    Create Day 2_Variables.C

---
+++ Day 2_Variables.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int age=21;
+float height=5.1;
+char name=Shama Fathima Shaik;
+printf("My Age is %d",age);
+printf("My Height is %f",height);
+printf("My Name is %c",name);
+return 0;
+}
